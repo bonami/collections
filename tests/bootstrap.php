3de15eb71166c651f3c254701c9339c5eac0b5e2
Jan Machala <jan.machala@email.cz>
2020-11-02 19:47:02 +0100

    Remove unused variable

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,4 @@
 
 declare(strict_types=1);
 
-/** @var \Composer\Autoload\ClassLoader $autoload */
-$autoload = require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
